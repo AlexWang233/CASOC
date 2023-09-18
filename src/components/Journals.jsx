9ca94d79d41fdf6ab916da5af5aa9c7955e9ee1b
AlexWang233 <alex13901350@gmail.com>
2023-09-17 20:01:28 -0400

    Components Init

---
+++ src/components/Journals.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Journals(props) {
+  return <div></div>;
+}
+
+export default Journals;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Navbar(props) {
+  return <div></div>;
+}
+
+export default Navbar;
