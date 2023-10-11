f3ff21a2c9b26129810ebdbb8f964edfafa9146c
AlexWang233 <alex13901350@gmail.com>
2023-10-11 05:28:31 -0400

    Terms of Service

--- src/components/Membership.jsx
+++ src/components/Membership.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import { useState } from "react";
 import Popup from "reactjs-popup";
 import "reactjs-popup/dist/index.css";
 
-function Membership(props) {
+const Membership = () => {
+  const [viewTerms, setViewTerms] = useState(false);
   return (
     <div className="membership">
       <Popup
@@ -22,74 +23,120 @@ function Membership(props) {
       >
         {(close) => (
           <div className="modal w-full h-full">
-            <form>
-              <label>
-                会员姓名：
-                <input type="text" name="name" />
-              </label>
-              <br />
-              <label>
-                毕业院校：
-                <input type="text" name="name" />
-              </label>
-              <br />
-              <label>
-                毕业年份：
-                <input type="text" name="name" />
-              </label>
-              <br />
-              <label>
-                居住地邮编：
-                <input type="text" name="name" />
-              </label>
-              <br />
-              <label>
-                生日 (Optional)：
-                <input type="text" name="name" />
-              </label>
-              <br />
-              <label>
-                性别 (Optional)：
-                <input type="text" name="name" />
-              </label>
-              <br />
-              <label>
-                家庭地址 (Optional)：
-                <input type="text" name="name" />
-              </label>
-              <br />
-              <label>
-                移动电话 (Optional)：
-                <input type="text" name="name" />
-              </label>
-              <br />
-              <label>
-                职业 (Optional)：
-                <input type="text" name="name" />
-              </label>
-              <br />
-              <label>希望社区能够提供的服务：</label> <br />
-              <textarea
-                rows={5}
-                cols={50}
-                wrap="soft"
-                placeholder="Add your text"
-              />
-              <br />
-              <label>希望能参加的社区活动：</label> <br />
-              <textarea
-                rows={5}
-                cols={50}
-                wrap="soft"
-                placeholder="Add your text"
-              />
-              <br />
-              <label className="w-10 mr-2">
-                <input type="checkbox" />
-                我同意服务条款
-              </label>
-              <input type="submit" value="提交" />
-            </form>
+            {!viewTerms ? (
+              <form>
+                <label>
+                  会员姓名：
+                  <input type="text" name="name" />
+                </label>
+                <br />
+                <label>
+                  毕业院校：
+                  <input type="text" name="name" />
+                </label>
+                <br />
+                <label>
+                  毕业年份：
+                  <input type="text" name="name" />
+                </label>
+                <br />
+                <label>
+                  居住地邮编：
+                  <input type="text" name="name" />
+                </label>
+                <br />
+                <label>
+                  生日 (Optional)：
+                  <input type="text" name="name" />
+                </label>
+                <br />
+                <label>
+                  性别 (Optional)：
+                  <input type="text" name="name" />
+                </label>
+                <br />
+                <label>
+                  家庭地址 (Optional)：
+                  <input type="text" name="name" />
+                </label>
+                <br />
+                <label>
+                  移动电话 (Optional)：
+                  <input type="text" name="name" />
+                </label>
+                <br />
+                <label>
+                  职业 (Optional)：
+                  <input type="text" name="name" />
+                </label>
+                <br />
+                <label>希望社区能够提供的服务：</label> <br />
+                <textarea
+                  rows={5}
+                  cols={50}
+                  wrap="soft"
+                  placeholder="Add your text"
+                />
+                <br />
+                <label>希望能参加的社区活动：</label> <br />
+                <textarea
+                  rows={5}
+                  cols={50}
+                  wrap="soft"
+                  placeholder="Add your text"
+                />
+                <br />
+                <button onClick={() => setViewTerms(true)}>查看服务条款</button>
+                <input type="submit" value="提交" />
+              </form>
+            ) : (
+              <div className="service-section flex flex-col justify-between m-4">
+                <h1>Terms of Service</h1>
+                <div>
+                  <h2>Consent：</h2>
+                  <p>
+                    I consent that the Company can provide their services and
+                    communicate with me via mobile phone, messages, e-mail and
+                    any kind of online communications, provided that these
+                    communications comply with privacy regulations.
+                  </p>
+                </div>
+                <div>
+                  <h2>Appointment Reminders, Reschedules and Cancellations</h2>
+                  <p>
+                    I understand that Company can reach me any time to remind me
+                    of my appointments or let me know in case of any change
+                    about my appointments. And I also understand that the
+                    Company can employ and use a third-party automated system to
+                    reach out me for the purpose of &quot;confirm&quot;,
+                    &quot;reschedule&quot; or &quot;cancel&quot;.
+                  </p>
+                </div>
+                <div>
+                  <h2>Contact Information Change</h2>
+                  <p>
+                    I accept that I am responsible of notifying the Company when
+                    my contact informations change.
+                  </p>
+                </div>
+                <div>
+                  <h2>Consent Cancellations</h2>
+                  <p>
+                    I know that I can revoke this consent at any time by
+                    contacting the Company.
+                  </p>
+                </div>
+                <div className="flex justify-center mb-4">
+                  <label className="w-40 mr-2">
+                    <input type="checkbox" />
+                    我同意服务条款
+                  </label>
+                  <button onClick={() => setViewTerms(false)}>
+                    返回注册页面
+                  </button>
+                </div>
+              </div>
+            )}
             <button className="close" onClick={close}>
               &times;
             </button>
@@ -101,6 +148,6 @@ function Membership(props) {
       </div>
     </div>
   );
-}
+};
 
 export default Membership;
