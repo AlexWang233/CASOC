b678a94c0f04773541e194a3a2f02d09db3913bb
AlexWang233 <alex13901350@gmail.com>
2023-10-13 02:15:01 -0400

    Logo Image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import logo from "../assets/logo.jpg";
 
 function Navbar(props) {
   return (
-    <div className="navbar w-full bg-sky-200 mb-4">
-      <div className="header">
+    <div className="relative w-full bg-sky-200 min-h-[136px]">
+      <img
+        src={logo}
+        alt="Logo"
+        className="absolute ml-2 top-1/2 transform -translate-y-1/2 max-w-[120px] max-h-[120px] "
+      />
+      <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <h2 className="subtitle">Chinese Alumni Society of Canada</h2>
         <h2 className="subtitle">WWW.CASOC.CA</h2>
         <h1 className="title">华校友联社区</h1>
