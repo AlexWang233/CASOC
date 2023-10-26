5826f6d119f5469f523ef00b6008cc32156a1a70
AlexWang233 <alex13901350@gmail.com>
2023-10-25 23:04:59 -0400

    Adjust Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,95 @@ import { footerList } from "../constants";
 
 const Footer = () => {
   return (
-    <div className="w-full min-h-[80px] flex justify-start items-center bg-[#bcaaa4]">
+    <div className="w-full min-h-[80px] flex justify-start items-center justify-center bg-[#bcaaa4]">
       {footerList.map((footerSection, index) => (
         <FooterSection props={footerSection} index={`footer-${index}`} />
       ))}
+      <Popup
+        modal
+        nested
+        trigger={
+          <button className="h-full flex items-center justify-center hover:bg-gray-500">
+            <div className="min-w-fit content-start text-center p-4 pl-8 pr-8">
+              Contact Us
+            </div>
+          </button>
+        }
+        contentStyle={{
+          width: "90%",
+          height: "fit-content",
+          maxWidth: "800px",
+        }}
+      >
+        {(close) => (
+          <div className="modal w-full h-fit p-4">
+            <h1 className="text-xl text-center">Contact Us</h1>
+            <form className="register-form flex flex-col justify-between m-4 gap-y-2 overflow-auto h-fit">
+              <div className="member-form-section">
+                <input
+                  type="text"
+                  id="floating_outlined"
+                  className="member-form-input peer"
+                  placeholder=" "
+                />
+                <label
+                  htmlFor="floating_outlined"
+                  className="member-form-label"
+                >
+                  First Name
+                </label>
+              </div>
+              <div className="member-form-section">
+                <input
+                  type="text"
+                  id="floating_outlined"
+                  className="member-form-input peer"
+                  placeholder=" "
+                />
+                <label
+                  htmlFor="floating_outlined"
+                  className="member-form-label"
+                >
+                  Last Name
+                </label>
+              </div>
+              <div className="member-form-section">
+                <input
+                  type="text"
+                  id="floating_outlined"
+                  className="member-form-input peer"
+                  placeholder=" "
+                />
+                <label
+                  htmlFor="floating_outlined"
+                  className="member-form-label"
+                >
+                  Email
+                </label>
+              </div>
+
+              <label
+                htmlFor="message"
+                className="member-form-textarea-label mt-4"
+              >
+                Message：
+              </label>
+              <textarea
+                id="message"
+                rows="4"
+                className="member-form-textarea"
+                placeholder="Write your thoughts here..."
+              ></textarea>
+              <div className="flex justify-center mt-2">
+                <input type="submit" value="Send" />
+              </div>
+            </form>
+            <button className="close" onClick={close}>
+              &times;
+            </button>
+          </div>
+        )}
+      </Popup>
     </div>
   );
 };
@@ -22,7 +107,9 @@ const FooterSection = ({ props }) => {
       nested
       trigger={
         <button className="h-full flex items-center justify-center hover:bg-gray-500">
-          <div className="min-w-fit content-start text-center p-4">{title}</div>
+          <div className="min-w-fit content-start text-center p-4 pl-8 pr-8">
+            {title}
+          </div>
         </button>
       }
       contentStyle={{
